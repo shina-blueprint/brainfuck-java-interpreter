8a5f5724a6316b654fd311bde53c5e48e771c656
shina <zodiac_oracle@docomo.ne.jp>
2022-07-17 01:25:38 +0900

    Add outline

--- brainfuck/src/main/java/Brainfuck.java
+++ brainfuck/src/main/java/Brainfuck.java
@@ -1,6 +1,24 @@
 public class Brainfuck {
 
+    static final char INCREMENT = '+';
+    static final char DECREMENT = '-';
+    static final char RIGHT = '>';
+    static final char LEFT = '<';
+    static final char LOOP_START = '[';
+    static final char LOOP_END = ']';
+    static final char OUTPUT = '.';
+    static final char INPUT = ',';
+
+    static final int MEMORY_SIZE = 1024;
+
     public static void main(String[] args) {
+        int[] memory;
+        int ptr = 0;
+        String code = "";
+        int codePtr = 0;
+        int codeLen = 0;
+
+        memory = new int[MEMORY_SIZE];
     }
 
 }
